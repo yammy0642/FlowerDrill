5fea5744bf957a0165b6369e3ddf46091a7a2b5c
Panchadi <yamuna.panchadi@accenture.com>
2023-09-20 19:12:01 +0530

    updated5

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  subscription_id = "3de54ce0-297b-4a95-b908-52facf8095dc"
 }
 
 resource "azurerm_resource_group" "example" {
